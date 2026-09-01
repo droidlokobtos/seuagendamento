bcf6cea5eed16342c50b8c382cc7b8075d8f5e66
droidlokobtos <wesleiwilliamemprego@gmail.com>
2026-09-01 14:28:26 -0300

    make lint compatible with legacy formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import js from "@eslint/js";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -36,7 +35,8 @@ export default tseslint.config(
       "@typescript-eslint/no-unused-vars": "off",
       // Dívida legada monitorada: novos módulos devem preferir unknown/tipos gerados.
       "@typescript-eslint/no-explicit-any": "warn",
+      "prefer-const": "warn",
+      "no-empty": "warn",
     },
   },
-  eslintPluginPrettier,
 );
